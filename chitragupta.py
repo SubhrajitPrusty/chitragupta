783512a429b73c38ea24bf8cd4652049a83fe499
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-09-11 23:42:23 +0530

    use grequests to send out all requests at the same time

--- chitragupta.py
+++ chitragupta.py
@@ -5,7 +5,8 @@ Author : Rameshwar Bhaskaran
 
 import requests
 import json
-import pywikibot
+#  import pywikibot
+import grequests
 
 ignored_repos = set({'git-exercise'})
 
@@ -13,38 +14,33 @@ def get_all_repos():
     # get a list of all repositories in the MetaKGP org
     r = requests.get("https://api.github.com/orgs/metakgp/repos")
 
+    all_contribs = set()
+
     if not r.ok:
         print "Looks like something is wrong"
         print r.status_code
         exit(0)
     else:
         repos = json.loads(r.text)
-        contributor_urls = {}
-        for repo in repos:
-            if repo['name'] not in ignored_repos:
-                contributor_urls[repo['name']] = repo['contributors_url']
-        contributors = {}
-        for repo, url in contributor_urls.items():
-            try:
-                print "Getting ", repo
-                # ignore the sandbox
-                contributor_response = requests.get(url)
-                contributors[repo] = set()
-                repo_contributors = json.loads(contributor_response.text)
-                for contributor in repo_contributors:
-                    contributors[repo].add(contributor['login'])
-            except Exception as e:
-                print "Failed due to ", str(e)
-                pass
+        repos = repos[:20]
 
-    consolidated_list = set()
-    for repo in contributors:
-        print repo, contributors[repo]
-        consolidated_list = consolidated_list.union(contributors[repo])
-    return contributors, consolidated_list
+        contrib_urls = [repo['contributors_url'] for repo in repos if repo['name'] not in ignored_repos]
+
+        all_reqs = (grequests.get(u) for u in contrib_urls)
+
+        all_resps = grequests.map(all_reqs)
+
+        for resp in all_resps:
+            if resp:
+                repo_contribs = json.loads(resp.text)
+                all_contribs = all_contribs.union([person['login'] for person in repo_contribs])
+
+        print all_contribs
+
+    return all_contribs
 
 def main():
-    contributors, consolidated_list = get_all_repos()
+    consolidated_list = get_all_repos()
 
     # Build the markup for the page
     text = "<b>List of contributors on Github:</b><br/><br/>"
@@ -56,12 +52,16 @@ def main():
         url = "https://github.com/%s" % github_username
         text += "%s. [%s %s] <br>" % (iter + 1, url, github_username)
 
+    filout = open('test.html', 'w')
+    filout.write(text)
+    filout.close()
+
     # Update the page with this markup
 
-    site = pywikibot.Site()
-    page = pywikibot.Page(site, u'MetaKGP_Github_Contributors')
-    page.text = text
-    page.save(u'Update list of contributors')
+    #  site = pywikibot.Site()
+    #  page = pywikibot.Page(site, u'MetaKGP_Github_Contributors')
+    #  page.text = text
+    #  page.save(u'Update list of contributors')
 
 if __name__ == "__main__":
     main()
